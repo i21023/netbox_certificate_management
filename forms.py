7bf5b00dcff9bfe1ba60f5174c1f7cc07f874f12
schmidtj <julian.schmidt@kern-liebers.com>
2024-08-13 08:47:09 +0200

    update dependencies in setup.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from netbox.forms import NetBoxModelForm
-
-from .models import Certificate
-
-class CertificateForm(NetBoxModelForm):
-    class Meta:
-        model=Certificate
-        fields='serial_number, signature_algorithm, issuer, '
--- setup.py
+++ setup.py
@@ -8,4 +8,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    install_requires=(
+        "cryptography>=43.0.0",
+        "requests>=2.32.3",
+        "django-mptt>=0.16.0"
+    )
 )
