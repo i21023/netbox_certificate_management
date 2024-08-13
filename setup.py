e7ae0655917e4f4d07bcc48b5cd915fbe7b45e5d
schmidtj <julian.schmidt@kern-liebers.com>
2024-08-13 09:26:16 +0200

    try fixing installation

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=(
+    install_requires=[
         "cryptography>=43.0.0",
         "requests>=2.32.3",
         "django-mptt>=0.16.0"
-    )
+    ]
 )
