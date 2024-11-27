79e72dfb99e315a4629702c6e9e40150e4d3402f
schmidtj <julian.schmidt@kern-liebers.com>
2024-11-27 16:14:30 +0100

    fix permissions of navigation entry

--- netbox_certificate_management/navigation.py
+++ netbox_certificate_management/navigation.py
@@ -3,7 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_certificate_management:certificate_list',
-        link_text=_('certificates')
+        link="plugins:netbox_certificate_management:certificate_list",
+        link_text=_("certificates"),
+        permissions=["netbox_certificate_management.view_certificate"],
     ),
 )
+
